b746e36dd7cae9be6cd2a55d7d53e24935207118
Frank <frankferrer88@icloud.com>
2022-09-19 15:27:51 -0600

    Initial Commit

---
+++ src/Graph.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Graph<T> {
+
+    // We use Hashmap to store the edges in the graph
+    private Map<T, List<T> > map = new HashMap<>();
+
+    // This function adds a new vertex to the graph
+    public void addVertex(T s)
+    {
+        map.put(s, new LinkedList<T>());
+    }
+
+    // This function adds the edge
+    // between source to destination
+    public void addEdge(T source,
+                        T destination,
+                        boolean bidirectional)
+    {
+
+        if (!map.containsKey(source))
+            addVertex(source);
+
+        if (!map.containsKey(destination))
+            addVertex(destination);
+
+        map.get(source).add(destination);
+        if (bidirectional == true) {
+            map.get(destination).add(source);
+        }
+    }
+
+    // Prints the adjancency list of each vertex.
+    @Override
+    public String toString()
+    {
+        StringBuilder builder = new StringBuilder();
+
+        for (T v : map.keySet()) {
+            builder.append(v.toString() + ": ");
+            for (T w : map.get(v)) {
+                builder.append(w.toString() + " ");
+            }
+            builder.append("\n");
+        }
+
+        return (builder.toString());
+    }
+}
---
+++ src/processtown.java
@@ -0,0 +1,61 @@
+import java.io.*;
+
+
+public class processtown
+{
+    public static void main(String[] args){
+        String argIn = "";
+        if(args.length >0) {
+            argIn = args[0];
+        }
+        Graph<Integer> g = new Graph<Integer>();
+
+        // edges are added.
+        // Since the graph is bidirectional,
+        // so boolean bidirectional is passed as true.
+        g.addEdge(0, 1, true);
+        g.addEdge(0, 4, true);
+        g.addEdge(1, 2, true);
+        g.addEdge(1, 3, true);
+        g.addEdge(1, 4, true);
+        g.addEdge(2, 3, true);
+        g.addEdge(3, 4, true);
+
+        // Printing the graph
+        System.out.print("Graph:\n"
+                + g.toString());
+
+        switch(argIn){
+            case "":
+                System.out.print("\"Test\"\n");
+                System.out.print("3,\"1 First Street\",\"2 First Street\"\n");
+                System.out.print("2,\"1 Second Street\",\"1 First Street\"\n");
+                System.out.print("3,\"2 First Street\",\"1 Second Street\"\n");
+                break;
+            case  "a":
+                // TODO Show the current town in alternate format.
+                break;
+            case  "r":
+                // TODO Open File
+                break;
+            case  "s":
+                break;
+            case  "v":
+                break;
+            case  "w":
+                break;
+            case  "h":
+                System.out.print("Syntax: h [-option [parameter]]\n");
+                System.out.print("  options:\n");
+                System.out.print("       s   show current town in standard format\n");
+                System.out.print("       a   show current town in alternate format\n");
+                System.out.print("       r   read town data from file identified by parameter\n");
+                System.out.print("       w   write current town to file identified by parameter\n");
+                System.out.print("       v   show version\n");
+                System.out.print("       h   help (this display)\n");
+                break;
+            default:
+                System.out.println("Argument not understood, enter h for options");
+        }
+    }
+}
